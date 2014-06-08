987fc0a274d137a9da2e27f8f1514325ad3aa305
Raymond Mendoza <raymendoza@ebay.com>
2014-06-07 22:12:57 -0400

    Using custom view controllers. Requires workarounds from http://stackoverflow.com/questions/24029586/xcode-6-storyboard-unwind-segue-with-swift-not-connecting-to-exit and also to delete the initializers.

---
+++ ToDoList/XYZAddToDoItemViewController.swift
@@ -0,0 +1,34 @@
+//
+//  XYZAddToDoItemViewController.swift
+//  ToDoList
+//
+//  Created by Mendoza, Ray on 6/7/14.
+//  Copyright (c) 2014 Mendoza, Ray. All rights reserved.
+//
+
+import UIKit
+
+class XYZAddToDoItemViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ XYZTodoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZTodoListTableViewController.h
+//  ToDoList
+//
+//  Created by Mendoza, Ray on 6/7/14.
+//  Copyright (c) 2014 Mendoza, Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZTodoListTableViewController
+- (IBAction)unwindToList: (UIStoryboardSegue *)segue;
+@end
